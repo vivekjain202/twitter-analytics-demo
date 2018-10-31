b481e140f60a3aa4da013047b9624b4fa22b3801
v.jain.202 <vivek.jain@mountblue.io>
2018-10-31 06:44:02 +0530

    component will update removed

--- src/App.js
+++ src/App.js
@@ -25,39 +25,8 @@ class App extends Component {
     this.func = this.func.bind(this);
   }
 
-  componentDidUpdate(){
-    setInterval(()=>{
-      console.log('here')
-      axios({
-        method: 'get',
-        url: 'https://jsonplaceholder.typicode.com/posts',
-        responseType: 'json'
-      })
-        .then(data => {
-          this.setState({ data: data.request.response });
-        })
-      return [];
-    },5000)
-  }
-
   func() {
     return this.state.data.map((e, i) => <h3 key={i}>{e.title}</h3>)
-  }
-  
-
-
-  componentWillUpdate() {
-    console.log('here')
-    // setInterval(()=>{
-    //   axios({
-    //     method : 'get',
-    //     url : 'https://jsonplaceholder.typicode.com/posts',
-    //     responseType : 'json'
-    //   })
-    //   .then(data=>{
-    //     this.setState({ data : data.request.response});
-    //   })
-    // },5000)  
   }
 
   render() {
