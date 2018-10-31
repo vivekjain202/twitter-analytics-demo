ae735b821da2bbbf34ca01b78dfa9201f6c31f43
v.jain.202 <vivek.jain@mountblue.io>
2018-10-31 06:14:47 +0530

    Updates

---
+++ precache-manifest.c9dfd26ba080b42fa89f2173d90141fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ad47f3a7ba2941b2b1",
+    "url": "/twitter-analytics-demo/static/js/runtime~main.42ad47f3.js"
+  },
+  {
+    "revision": "1cdff0c785be2ae4be13",
+    "url": "/twitter-analytics-demo/static/js/main.1cdff0c7.chunk.js"
+  },
+  {
+    "revision": "79fa00b132c59fe1cffe",
+    "url": "/twitter-analytics-demo/static/js/1.79fa00b1.chunk.js"
+  },
+  {
+    "revision": "1cdff0c785be2ae4be13",
+    "url": "/twitter-analytics-demo/static/css/main.3f778d64.chunk.css"
+  },
+  {
+    "revision": "55a383073f57726f9490f1da192b68d5",
+    "url": "/twitter-analytics-demo/index.html"
+  }
+];
