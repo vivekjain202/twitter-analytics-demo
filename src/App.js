17f5a23b4a446dac282a697f3da4218896bc5634
v.jain.202 <vivek.jain@mountblue.io>
2018-10-31 06:13:33 +0530

    home page demo components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/HomePage';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    // component will mount is depricated so it's suggested to do this in intial setstate in constructor
+    // IIFE is used for data sanitization
+    (() => {
+      axios({
+        method: 'get',
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        responseType: 'json'
+      })
+        .then(data => {
+          this.setState({ data: data.request.response });
+        })
+      return [];
+    })();
+    this.state = {
+      data: [],
+    }
+    this.func = this.func.bind(this);
+  }
+
+  componentDidUpdate(){
+    setInterval(()=>{
+      console.log('here')
+      axios({
+        method: 'get',
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        responseType: 'json'
+      })
+        .then(data => {
+          this.setState({ data: data.request.response });
+        })
+      return [];
+    },5000)
+  }
+
+  func() {
+    return this.state.data.map((e, i) => <h3 key={i}>{e.title}</h3>)
+  }
+  
+
+
+  componentWillUpdate() {
+    console.log('here')
+    // setInterval(()=>{
+    //   axios({
+    //     method : 'get',
+    //     url : 'https://jsonplaceholder.typicode.com/posts',
+    //     responseType : 'json'
+    //   })
+    //   .then(data=>{
+    //     this.setState({ data : data.request.response});
+    //   })
+    // },5000)  
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HomePage />
+        {this.func()}
       </div>
     );
   }
